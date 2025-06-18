d51712ebb37535ce2bf7616a4e8053fe9c4ff63e
yuubd <gc.kim93@hotmail.com>
2025-06-18 15:32:14 -0700

    added: profile button

--- src/components/core/login.tsx
+++ src/components/core/login.tsx
@@ -8,6 +8,7 @@ import Typography from "@mui/material/Typography";
 import { User } from "@phosphor-icons/react/dist/ssr/User";
 import { paths } from "@/paths";
 import { getAppUrl } from "@/lib/get-app-url";
+import Avatar from "@mui/material/Avatar";
 
 export function Login(): React.JSX.Element | null {
   const { user, isLoading } = useUser();
@@ -17,7 +18,7 @@ export function Login(): React.JSX.Element | null {
   const href = user
     ? `${paths.auth.auth0.signOut}?returnTo=${encodeURIComponent(returnToUrl)}`
     : paths.auth.auth0.signIn;
-  const label = user ? "Sign out" : "Log in";
+  const label = user ? "Sign out" : "Sign in";
   
   return (
     <Box 
@@ -28,6 +29,47 @@ export function Login(): React.JSX.Element | null {
         borderColor: 'var(--NavItem-children-border)'
       }}
     >
+      {/* Profile button */}
+      <Box
+        component={user ? 'a' : 'div'}
+        href={user ? '/profile' : undefined}
+        sx={{
+          cursor: user ? 'pointer' : 'default',
+          textDecoration: 'none',
+          display: 'block',
+          mb: 1,
+          '&:hover': {
+            backgroundColor: user ? 'var(--NavItem-hover-background)' : undefined,
+          }
+        }}
+      >
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{
+            alignItems: 'center',
+            borderRadius: 1,
+            color: 'var(--NavItem-color)',
+            px: 2,
+            py: 1.5,
+            '&:hover': {
+              color: user ? 'var(--NavItem-hover-color)' : undefined
+            }
+          }}
+        >
+          <Avatar sx={{ width: 28, height: 28, bgcolor: '#1565c0', fontWeight: 600, fontSize: 16 }}>
+          {user && user.name ? user.name[0].toUpperCase() : <User color="var(--NavItem-icon-color)" weight="regular" size={20} />}
+
+          </Avatar>
+          <Typography
+            color="inherit"
+            variant="subtitle2"
+            sx={{ fontSize: "0.875rem", fontWeight: 500, lineHeight: "28px" }}
+          >
+            Profile
+          </Typography>
+        </Stack>
+      </Box>
       <Box
         component="a"
         href={href}
@@ -47,7 +89,7 @@ export function Login(): React.JSX.Element | null {
             alignItems: 'center',
             borderRadius: 1,
             color: 'var(--NavItem-color)',
-            px: 2,
+            px: '21px',
             py: 1.5,
             '&:hover': {
               color: 'var(--NavItem-hover-color)'
@@ -62,6 +104,7 @@ export function Login(): React.JSX.Element | null {
             color="inherit"
             variant="subtitle2"
             sx={{ 
+              px: '6px',
               fontSize: "0.875rem", 
               fontWeight: 500, 
               lineHeight: "28px"
